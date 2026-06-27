16eba4052f6f135c400a0d36018083ca21eca8df
91wan <lcx115478303@gmail.com>
2026-06-27 20:04:07 +0800

    test(preview): isolate error view test on main actor

--- PreviewExtensionTests/PreviewErrorViewTests.swift
+++ PreviewExtensionTests/PreviewErrorViewTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 
 final class PreviewErrorViewTests: XCTestCase {
+    @MainActor
     func testErrorViewContainsVisibleTextControls() {
         let view = PreviewErrorView(title: "Preview unavailable", message: "The file is not UTF-8.")
 
